c65f6724150f8c5815ff1a58696c1f658d343baa
Marcelo Carvalho <marceloodev@gmail.com>
2025-02-22 13:44:23 -0300

    Adicionado seção de ofertas com estilo e integração na HomePage

---
+++ src/components/OfertSection/index.jsx
@@ -0,0 +1,33 @@
+import "./SectionOfert.css";
+import { ButtonRosa } from "../Buttons/index";
+import { useEffect } from "react";
+import { useLocation, NavLink } from "react-router-dom";
+import sapato from "../../../public/sapato.png";
+import fundo_sapato from "../../../public/fundo_sapato.png";
+
+export function OfertSection() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return (
+    <div className="container-ofert">
+      <div className="image-ofert">
+        <img src={sapato} alt="Sapato" className="sapato" />
+        <img src={fundo_sapato} alt="Fundo Sapato" className="fundo-sapato" />
+      </div>
+      <div className="conteudo-ofert">
+        <span className="title-ofert">Oferta especial</span>
+        <h2>Air Jordan edição de colecionador</h2>
+        <p className="lorem-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        </p>
+        <NavLink to="/viewProduct">
+          <ButtonRosa />
+        </NavLink>
+      </div>
+    </div>
+  );
+}
