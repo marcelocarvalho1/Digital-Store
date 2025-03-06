4246b338351b98ec2f5d299286f75f1e55ccf1e0
Marcelo Carvalho <marceloodev@gmail.com>
2025-03-06 11:46:12 -0300

    Adicionando página de criação de conta com componentes de cabeçalho e formulário e rotas correspondentes

---
+++ src/components/Header2/index.jsx
@@ -0,0 +1,12 @@
+import "./Header2.css";
+import { Logo } from "../Logo";
+
+export function Header2() {
+  return (
+    <header className="header2">
+      <div className="cabelhaco">
+        <Logo />
+      </div>
+    </header>
+  );
+}
