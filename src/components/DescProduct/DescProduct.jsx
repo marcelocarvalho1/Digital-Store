d6fb41adf99a24f4e375071a3a7d697f595611f8
Marcelo Carvalho <marceloodev@gmail.com>
2025-03-05 21:55:15 -0300

    Adicionando componentes de estrelas, seleção de cores e botões de tamanho, além de ajustes de estilo e dependências

--- src/components/DescProduct/DescProduct.jsx
+++ src/components/DescProduct/DescProduct.jsx
@@ -1,39 +1,54 @@
 import './DescProduct.css';
 import React, { useState } from 'react';
-// import Star from '../Stars/Stars';
-// import { FaStar } from 'react-icons/fa';
-// import ColorShoes from '../ColorsShoes/ColorsShoes';
-// import SizeButtonShoes from '../SizeButtonShoes/SizeButtonShoes';
-import Tenis1 from '../../../public/TenisSlide.svg';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import Star from '../Stars/Stars';
+import { FaStar } from 'react-icons/fa';
+import ColorShoes from '../ColorsShoes/ColorsShoes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import SizeButtonShoes from '../SizeButtonShoes/SlideShoes';
 
-const coresFundo = ['#E2E3FF', '#FFE8BC', '#FFC0BC', '#DEC699', '#E8DFCF'];
+import Tenis1 from '../../../public/detalhes.png';
+
+const coresFundo = [
+    { cor: '#E2E3FF' },
+    { cor: '#FFE8BC' },
+    { cor: '#FFC0BC' },
+    { cor: '#DEC699' },
+    { cor: '#E8DFCF' }
+];
 
 export function DescProduct() {
     const [indiceCor, setIndiceCor] = useState(0);
 
-    const proximaCor = () => setIndiceCor((indiceCor + 1) % coresFundo.length);
-    const anteriorCor = () => setIndiceCor((indiceCor - 1 + coresFundo.length) % coresFundo.length);
+    const proximaCor = () => {
+        setIndiceCor((indiceCor + 1) % coresFundo.length);
+    };
+
+    const anteriorCor = () => {
+        setIndiceCor((indiceCor - 1 + coresFundo.length) % coresFundo.length);
+    };
 
     return (
-        <div className='color-background' style={{ backgroundColor: coresFundo[indiceCor] }}>
+        <div className='color-background'>
             <div className='containerDP'>
                 <div className='caminhoTenis'>
                     <h5>Home / Produtos / Tênis / Nike / Tênis Nike Revolution</h5>
                 </div>
                 <div className='descricaoProduto'>
                     <div className='imgDescProd'>
-                        <div className='imagemGrande'>
-                            <img src={Tenis1} alt="Tênis Nike Revolution" />
+                        <div className='imagemGrande' style={{ backgroundColor: coresFundo[indiceCor].cor }}>
+                            <img src={Tenis1} alt="" />
+                            {/* Botões de navegação */}
                             <div className='btn-slide'>
                                 <FontAwesomeIcon icon={faChevronLeft} className="botaoNavegacao" onClick={anteriorCor} />
                                 <FontAwesomeIcon icon={faChevronRight} className="botaoNavegacao" onClick={proximaCor} />
+              
                             </div>
                         </div>
+                        {/* Miniaturas de imagens (alteram o slide de cor) */}
                         <div className='miniImagens'>
                             {coresFundo.map((cor, index) => (
-                                <img key={index} src={Tenis1} alt={`Miniatura ${index + 1}`} onClick={() => setIndiceCor(index)} />
+                                <img key={index} onClick={() => setIndiceCor(index)} src={Tenis1} className={`item${index + 1}`} alt="" />
                             ))}
                         </div>
                     </div>
@@ -49,13 +64,21 @@ export function DescProduct() {
                         <h4 className='referenciasProduto'>Descrição do Produto</h4>
                         <p className='referenciasProduto'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, autem nesciunt molestiae est vel perferendis!</p>
                         <h4 className='referenciasProduto'>Tamanho</h4>
-                        <div className='tamanhoSapato'><SizeButtonShoes /></div>
+                        <div className='tamanhoSapato'>
+                            <SizeButtonShoes/>
+                        </div>
                         <h4 className='referenciasProduto'>Cores</h4>
-                        <div className='cores'><ColorShoes /></div>
-                        <button className='btn-comprar'>Comprar</button>
+                        <div className='cores'>
+                            <ColorShoes />
+                        </div>
+                        <div>
+                            <button className='btn-comprar'>Comprar</button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    );
+    )
 }
+
+export default DescProduct;
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import { HomePage } from '../pages/HomePage'
-import { ProductListingPage } from '../pages/ProductListingPage/ProductListingPage'
-import { ProductViewPage } from '../pages/ProductViewPage/ProductViewPage'
+import { ProductListingPage } from '../pages/ProductListingPage'
+import { ProductViewPage } from '../pages/ProductViewPage'
 
 export function AppRoutes() {
     return (
